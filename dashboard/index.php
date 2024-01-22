3300daf31d06a80bce312cfd7b622909e7ea8409
Stark <95195608+Mndvv@users.noreply.github.com>
2024-01-22 18:58:45 +0700

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Something Went Wrong</title>
+	<title>RockstoneAIO</title>
 	<style>
 		@font-face{
 		margin: auto;
